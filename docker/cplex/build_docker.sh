37a04ffac28c5d351e503e30516afd8881a0976c
dm <diogodpmacedo@gmail.com>
2021-01-06 16:01:20 +0000

    done ex5

--- docker/cplex/build_docker.sh
+++ docker/cplex/build_docker.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-docker build -t mbinf_um/bs2020_cplex_me_toolset:1.1 -t mbinf_um/bs2020_cplex_me_toolset:latest .
+docker build -t mbinf_um/bs2020_cplex_me_toolset:1.2 -t mbinf_um/bs2020_cplex_me_toolset:latest .
